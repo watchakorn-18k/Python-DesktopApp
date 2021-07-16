d78fd4edbdd0a6e74916367c615378f10c63afce
watchakorn-18k <porton2559@gmail.com>
2021-07-16 23:06:52 +0700

    ถึงตอนที่สร้างฟังค์ชั่นในปุ่มกด

--- main.py
+++ main.py
@@ -1,10 +1,44 @@
 # -*- encoding: utf-8 -*-
 
 from kivy.app import App
+from kivy.metrics import dp
+from kivy.properties import StringProperty
+from kivy.uix.stacklayout import StackLayout
+from kivy.uix.gridlayout import GridLayout
 from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.widget import Widget
+
+
+class WidgetsExample(GridLayout):
+    count = 0
+    my_text = StringProperty("ฮาโหลววววว")
+    def on_button_click(self):
+        print("กดแล้วนะ")
+        self.count += 1
+        self.my_text = f"{self.count}"
+
+
+
+
+class StackLayoutExample(StackLayout):
+    def __init__(self,**kwargs):
+        super().__init__(**kwargs)
+        for i in range(0,100):
+            #size = dp(100) + i *10
+            size = dp(100) 
+            b = Button(
+                    text=str(i+1), 
+                    size_hint=(None,None),
+                    size=(size,size)
+                )
+            self.add_widget(b)
+        
+
+#class GridLayoutExample(GridLayout):
+#   pass
+
 
 
 class AnchorLayoutSample(AnchorLayout):
